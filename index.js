8dafe8ee31dca6b54e40eaca3d1e2d58b0a990c7
SamuTechMan <117929531+SamuTechMan@users.noreply.github.com>
2023-05-26 01:04:06 -0300

    added new functions

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ let image = document.getElementById("sus");
 function quebrar() {
   if (i >= 3) {
     image.src = "/img/quebrada.png";
+    document.querySelector('button[onclick="apagar()"]').disabled = true;
+    document.querySelector('button[onclick="acender()"]').disabled = true;
+    document.querySelector('button[onclick="darkFunction()"]').disabled = true;
+    document.querySelector('button[onclick="acender()"]').disabled = true;
   } else {
     i++;
   }
+}
+
+function acender() {
+  element = document.getElementById("sus");
+  element = image.src = "/img/acesa.png";
+}
+
+function apagar() {
+  element = document.getElementById("sus");
+  element = image.src = "/img/apagada.png";
+}
+
+function resetar() {
+  let botoes = document.querySelectorAll(".button");
+  botoes.forEach((botao) => {
+    botao.disabled = false;
+  });
+  i = 0;
+  image.src = "/img/apagada.png";
 }
 
 function darkFunction() {
